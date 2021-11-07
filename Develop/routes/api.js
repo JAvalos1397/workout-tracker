f3751181aba7cc3105763ffe5af42044abd76216
Jose Avalos <avaljose@hotmai.com>
2021-11-07 12:46:38 -0600

    working on routes'

---
+++ Develop/routes/api.js
@@ -0,0 +1,70 @@
+const router = require('express').Router();
+const path = require('path');
+const Workout = require("../models/workout")
+
+router.get("/api/workouts", (req, res)=> {
+    Workout.aggregate([
+        {
+            $addFields: {
+                totalDuration:{
+                    $sum: '$exercises.duration'
+                }
+            }
+        }
+    ])
+    .then(data =>{
+        res.json(data)
+    })
+    .catch(er => {
+        res.status(500).json({message:"Workout not found"})
+    })
+
+})//end get
+
+router.put("/api/workouts/:id", (req,res)=>{
+    console.log(req.body)
+    Workout.findByIdAndUpdate(req.params.id, {
+        $push: {
+            exercises:req.body,
+        }
+    })
+    .then(data => {
+        res.json(data)
+    })
+    .catch(er => {
+        res.status(500).json({message:"workout not added"})
+    })
+})//end put
+
+router.post("/api/workouts", (req, res)=> {
+    console.log(req.body, 'this is create excersive')
+    Workout.create({})
+    .then(data=>{
+        res.json(data)
+    })
+    .catch(er=>{
+        res.status(500).json({message:"workout not added"})
+    })
+
+})// end post 
+
+router.get("/api/workouts/range", (req,res) =>{
+    Workout.aggregate([{
+        $addFields:{
+            totalDuration: {
+                $sum:'$exercises.duration'
+            }
+        }
+    }])
+    .sort({_id:-1})
+    .limit(7)
+    .then(data=>{
+        res.json(data)
+    })
+    .catch(er=>{
+        res.status(500).json({message:"Range not found"})
+    })
+})
+
+
+module.exports = router;
---
+++ Develop/server.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+const express = require('express')
+const morgan = require('morgan')
+const htmlRoutes= require('./routes/view')
+const apiroutes = require('./routes/api')
+const port = process.env.PORT || 3001
+
+const app = express()
+
+app.use(express.json())
+
+app.use(express.urlencoded({extended:true}))
+
+app.use(morgan('dev'))
+
+app.use(express.static('public'))
+
+mongoose.connect('mongodb://localhost/workout', {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
+app.use(htmlRoutes)
+
+app.use(apiroutes)
+
+app.listen(port,() => {
+    console.log('running on port 3001')
+})
